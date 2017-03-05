a3c9a310416d878b6d53a69a04266a06f90c59e9
Hariharan Narayanan <enhariharan@gmail.com>
2017-03-05 14:50:23 +0530

    Added RChilli resume parser API key details

--- configuration.js
+++ configuration.js
@@ -7,6 +7,13 @@ var configuration = {
     test: {connectionString: 'mongodb://localhost/jobumes-test',},
     production: {connectionString: 'mongodb://produser:resudorp@ds145158.mlab.com:45158/jobumes-prod',},
   },
+  resumeParser: {
+    serviceUrl: 'http://java.rchilli.com/RChilliParser/services/RChilliParser?wsdl',
+    userKey: 'YOABEO3303Q',
+    version: '6.0.0',
+    subUserId: 'immaculateit',
+    timeout: 120 * 1000
+  },
 };
 
 module.exports = {configuration};
