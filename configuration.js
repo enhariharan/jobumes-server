6032ee8f0903d7dd195c2f5b35bf2fac293ae1c9
Hariharan Narayanan <enhariharan@gmail.com>
2017-03-09 20:09:12 +0530

    Added RChilli SOAP and REST API URLs

--- configuration.js
+++ configuration.js
@@ -8,7 +8,8 @@ var configuration = {
     production: {connectionString: 'mongodb://produser:resudorp@ds145158.mlab.com:45158/jobumes-prod',},
   },
   resumeParser: {
-    serviceUrl: 'http://java.rchilli.com/RChilliParser/services/RChilliParser?wsdl',
+    serviceUrlSoap: 'http://java.rchilli.com/RChilliParser/services/RChilliParser?wsdl',
+    serviceUrlRest: 'http://rest.rchilli.com/RChilliParser/Rchilli',
     userKey: 'YOABEO3303Q',
     version: '6.0.0',
     subUserId: 'immaculateit',
