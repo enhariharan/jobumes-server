5ded02b1497fecfe39d6e52d8ac58083a27b7bb2
Hariharan Narayanan <enhariharan@gmail.com>
2017-03-09 21:56:09 +0530

    Added configuration params for jobs

--- configuration.js
+++ configuration.js
@@ -15,6 +15,14 @@ var configuration = {
     subUserId: 'immaculateit',
     timeout: 120 * 1000
   },
+  jdParser: {
+    serviceUrlSoap: 'http://immaculateitjd.rchilli.com/JDParser/services/JDParser?wsdl',
+    serviceUrlRest: '',
+    userKey: 'YOABEO3303Q',
+    version: '2.0',
+    subUserId: 'RedGumm',
+    timeout: 120 * 1000
+  },
 };
 
 module.exports = {configuration};
