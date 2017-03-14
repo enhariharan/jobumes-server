daa0737c0ebb27859387146e0509ede820117ca9
Hariharan Narayanan <enhariharan@gmail.com>
2017-03-14 21:36:34 +0530

    Finished /parseResume integration

--- configuration.js
+++ configuration.js
@@ -8,9 +8,6 @@ var configuration = {
     production: {connectionString: 'mongodb://produser:resudorp@ds145158.mlab.com:45158/jobumes-prod',},
   },
   resumeParser: {
-    // serviceUrlSoap: 'http://java.rchilli.com/RChilliParser/services/RChilliParser?wsdl',
-    // serviceUrlRest: 'http://redgummi1.rchilli.com/RChilliParser/Rchilli/parseResumeBinary',
-    // serviceUrl: 'http://redgummi1.rchilli.com/RChilliParser/Rchilli/parseResumeBinary',
     serviceUrl: 'http://redgummi1.rchilli.com/RChilliParser/Rchilli',
     userKey: 'YOABEO3303Q',
     version: '7.0.0',
@@ -18,8 +15,6 @@ var configuration = {
     timeout: 120 * 1000
   },
   jdParser: {
-    // serviceUrlSoap: 'http://redgummijd.rchilli.com/JDParser/services/JDParser?wsdl',
-    // serviceUrlRest: '',
     serviceUrl: 'http://redgummijd.rchilli.com/JDParser/services/JDParser?wsdl',
     userKey: 'YOABEO3303Q',
     version: '2.0',
