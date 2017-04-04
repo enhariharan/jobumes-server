65c65c5f30832a50ade14a0ef4612e97367fc033
Hariharan Narayanan <enhariharan@gmail.com>
2017-04-04 05:57:02 +0530

    removed a few unwanted require calls

--- test/canary-test.js
+++ test/canary-test.js
@@ -1,6 +1,4 @@
 var expect = require('chai').expect;
-var describe = require('chai').describe;
-var it = require('chai').it;
 
 describe('jobumes server tests', () => {
   it('should pass the basic test setup test - Canary test', () => {
